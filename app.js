3586db162a56e4804ff0f21f650320b23bb9f267
ramteke3yash <ramteke.yashkun@gmail.com>
2023-06-17 18:47:42 +0530

    newly added code

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 // Get the form element
 const form = document.getElementById("my-form");
 
+// Function to edit a user
+function editUser(index) {
+  // Retrieve the user details from local storage
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+
+  // Get the user at the specified index
+  const user = users[index];
+
+  // Populate the form fields with the user data
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const phoneInput = document.getElementById("phone");
+  nameInput.value = user.name;
+  emailInput.value = user.email;
+  phoneInput.value = user.phone;
+
+  // Store the edit mode and index in the form's dataset
+  form.dataset.editMode = "true";
+  form.dataset.editIndex = index;
+}
+
 // Add event listener for form submission
 form.addEventListener("submit", function (e) {
   e.preventDefault(); // Prevent form submission
@@ -12,18 +33,21 @@ form.addEventListener("submit", function (e) {
   const name = nameInput.value;
   const email = emailInput.value;
   const phone = phoneInput.value;
-  // Create an object to store the user details
-  const user = {
-    name: name,
-    email: email,
-    phone: phone,
-  };
 
   // Get existing users from local storage
   let users = JSON.parse(localStorage.getItem("users")) || [];
 
-  // Add the new user to the array
-  users.push(user);
+  if (form.dataset.editMode === "true") {
+    // Editing an existing user
+    const editIndex = form.dataset.editIndex;
+    users[editIndex] = { name, email, phone };
+    form.dataset.editMode = "false";
+    form.dataset.editIndex = "";
+  } else {
+    // Creating a new user
+    const user = { name, email, phone };
+    users.push(user);
+  }
 
   // Store the updated users array in local storage
   localStorage.setItem("users", JSON.stringify(users));
@@ -73,6 +97,14 @@ function loadUserList() {
       )
     );
 
+    // Create the Edit button
+    const editButton = document.createElement("button");
+    editButton.innerHTML = "Edit";
+    editButton.addEventListener("click", function () {
+      editUser(index);
+    });
+    li.appendChild(editButton);
+
     // Create delete button for each user
     const deleteButton = document.createElement("button");
     deleteButton.innerHTML = "Delete";
@@ -82,7 +114,6 @@ function loadUserList() {
 
     // Append delete button to the list item
     li.appendChild(deleteButton);
-
     // Append list item to the user list
     userList.appendChild(li);
   });
@@ -92,20 +123,3 @@ function loadUserList() {
 document.addEventListener("DOMContentLoaded", function () {
   loadUserList();
 });
-
-// // Load existing users from local storage
-// document.addEventListener("DOMContentLoaded", function () {
-//   let users = JSON.parse(localStorage.getItem("users")) || [];
-//   const userList = document.getElementById("users");
-
-//   // Display each user in the user list
-//   users.forEach(function (user) {
-//     const li = document.createElement("li");
-//     li.appendChild(
-//       document.createTextNode(
-//         `Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}`
-//       )
-//     );
-//     userList.appendChild(li);
-//   });
-// });
