578216811c12dcb2358da78746f5cc52f6716191
ramteke3yash <ramteke.yashkun@gmail.com>
2023-07-06 20:04:00 +0530

    new code added by using crudcrud

--- app.js
+++ app.js
@@ -34,23 +34,40 @@ form.addEventListener("submit", function (e) {
   const email = emailInput.value;
   const phone = phoneInput.value;
 
-  // Get existing users from local storage
-  let users = JSON.parse(localStorage.getItem("users")) || [];
-
   if (form.dataset.editMode === "true") {
     // Editing an existing user
     const editIndex = form.dataset.editIndex;
-    users[editIndex] = { name, email, phone };
-    form.dataset.editMode = "false";
-    form.dataset.editIndex = "";
+    const user = { name, email, phone };
+    axios
+      .put(
+        `https://crudcrud.com/api/d31be6839a4d404aa382199dbb392eeb/appointment/${editIndex}`,
+        user
+      )
+      .then((response) => {
+        console.log(response.data);
+        // Reload the user list
+        loadUserList();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   } else {
     // Creating a new user
     const user = { name, email, phone };
-    users.push(user);
-  }
 
-  // Store the updated users array in local storage
-  localStorage.setItem("users", JSON.stringify(users));
+    axios
+      .post(
+        "https://crudcrud.com/api/d31be6839a4d404aa382199dbb392eeb/appointment",
+        user
+      )
+      .then((response) => {
+        console.log(response.data);
+        loadUserList();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   // Clear the input fields
   nameInput.value = "";
@@ -60,9 +77,6 @@ form.addEventListener("submit", function (e) {
   // Display a success message
   const msg = document.querySelector(".msg");
   msg.innerHTML = "Submitted";
-
-  // Reload the user list
-  loadUserList();
 });
 
 // Function to delete a user
@@ -82,44 +96,53 @@ function deleteUser(index) {
 
 // Function to load existing users from local storage and display them
 function loadUserList() {
-  let users = JSON.parse(localStorage.getItem("users")) || [];
-  const userList = document.getElementById("users");
+  axios
+    .get(
+      "https://crudcrud.com/api/d31be6839a4d404aa382199dbb392eeb/appointment"
+    )
+    .then((response) => {
+      const users = response.data;
+      const userList = document.getElementById("users");
 
-  // Clear the user list
-  userList.innerHTML = "";
+      // Clear the user list
+      userList.innerHTML = "";
 
-  // Display each user in the user list
-  users.forEach(function (user, index) {
-    const li = document.createElement("li");
-    li.appendChild(
-      document.createTextNode(
-        `Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}`
-      )
-    );
+      // Display each user in the user list
+      users.forEach(function (user, index) {
+        const li = document.createElement("li");
+        li.appendChild(
+          document.createTextNode(
+            `Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}`
+          )
+        );
 
-    // Create the Edit button
-    const editButton = document.createElement("button");
-    editButton.innerHTML = "Edit";
-    editButton.addEventListener("click", function () {
-      editUser(index);
+        // Create the Edit button
+        const editButton = document.createElement("button");
+        editButton.innerHTML = "Edit";
+        editButton.addEventListener("click", function () {
+          editUser(index);
+        });
+        li.appendChild(editButton);
+
+        // Create delete button for each user
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete";
+        deleteButton.addEventListener("click", function () {
+          deleteUser(index);
+        });
+
+        // Append delete button to the list item
+        li.appendChild(deleteButton);
+        // Append list item to the user list
+        userList.appendChild(li);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    li.appendChild(editButton);
-
-    // Create delete button for each user
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    deleteButton.addEventListener("click", function () {
-      deleteUser(index);
-    });
-
-    // Append delete button to the list item
-    li.appendChild(deleteButton);
-    // Append list item to the user list
-    userList.appendChild(li);
-  });
 }
 
-// Load existing users from local storage and display them
+// Load existing users from the API and display them
 document.addEventListener("DOMContentLoaded", function () {
   loadUserList();
 });
