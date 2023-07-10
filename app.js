393610abb8f0ee6727692e9c84dac8d4d58e854c
ramteke3yash <ramteke.yashkun@gmail.com>
2023-07-10 17:02:30 +0530

    new code added with edit and delete functionality

--- app.js
+++ app.js
@@ -1,67 +1,102 @@
+const API_BASE_URL =
+  "https://crudcrud.com/api/2565a4377db345d6a8c9062c8ffe89fd/appointment2";
+
 // Get the form element
 const form = document.getElementById("my-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const userList = document.getElementById("users");
+let editId = null;
 
 // Function to edit a user
-function editUser(index) {
-  // Retrieve the user details from local storage
-  let users = JSON.parse(localStorage.getItem("users")) || [];
-
-  // Get the user at the specified index
-  const user = users[index];
-
-  // Populate the form fields with the user data
-  const nameInput = document.getElementById("name");
-  const emailInput = document.getElementById("email");
-  const phoneInput = document.getElementById("phone");
+function editUser(user) {
   nameInput.value = user.name;
   emailInput.value = user.email;
   phoneInput.value = user.phone;
+  editId = user._id;
+}
 
-  // Store the edit mode and index in the form's dataset
-  form.dataset.editMode = "true";
-  form.dataset.editIndex = index;
+// Function to delete a user
+async function deleteUser(id) {
+  try {
+    const response = await axios.delete(`${API_BASE_URL}/${id}`);
+
+    //console.log("success");
+    loadUserList(); // Reload the user list
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Function to load existing users from the API and display them
+async function loadUserList() {
+  try {
+    const response = await axios.get(API_BASE_URL);
+
+    const users = response.data;
+
+    // Clear the user list
+    userList.innerHTML = "";
+
+    // Display each user in the user list
+    users.forEach(function (user, index) {
+      const li = document.createElement("li");
+      li.textContent = `Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}`;
+
+      // Create the Edit button
+      const editButton = document.createElement("button");
+      editButton.textContent = "Edit";
+      editButton.addEventListener("click", function () {
+        editUser(user);
+      });
+      li.appendChild(editButton);
+
+      // Create delete button for each user
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function () {
+        deleteUser(user._id);
+      });
+
+      // Append delete button to the list item
+      li.appendChild(deleteButton);
+      // Append list item to the user list
+      userList.appendChild(li);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // Add event listener for form submission
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async function (e) {
   e.preventDefault(); // Prevent form submission
 
   // Get the input values
-  const nameInput = document.getElementById("name");
-  const emailInput = document.getElementById("email");
-  const phoneInput = document.getElementById("phone");
   const name = nameInput.value;
   const email = emailInput.value;
   const phone = phoneInput.value;
 
-  if (form.dataset.editMode === "true") {
+  if (editId) {
     // Editing an existing user
-    const editIndex = form.dataset.editIndex;
+    //const editIndex = form.dataset.editIndex;
     const user = { name, email, phone };
-    axios
-      .put(
-        `https://crudcrud.com/api/d31be6839a4d404aa382199dbb392eeb/appointment/${editIndex}`,
-        user
-      )
-      .then((response) => {
-        console.log(response.data);
-        // Reload the user list
-        loadUserList();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      const response = await axios.put(`${API_BASE_URL}/${editId}`, user);
+
+      // Reload the user list
+      loadUserList();
+    } catch (err) {
+      console.log(err);
+    }
   } else {
     // Creating a new user
     const user = { name, email, phone };
-
     axios
-      .post(
-        "https://crudcrud.com/api/d31be6839a4d404aa382199dbb392eeb/appointment",
-        user
-      )
+      .post(API_BASE_URL, user)
       .then((response) => {
-        console.log(response.data);
+        //console.log(response.data);
         loadUserList();
       })
       .catch((err) => {
@@ -76,73 +111,8 @@ form.addEventListener("submit", function (e) {
 
   // Display a success message
   const msg = document.querySelector(".msg");
-  msg.innerHTML = "Submitted";
+  msg.textContent = "Submitted";
 });
-
-// Function to delete a user
-function deleteUser(index) {
-  // Get existing users from local storage
-  let users = JSON.parse(localStorage.getItem("users")) || [];
-
-  // Remove the user at the specified index
-  users.splice(index, 1);
-
-  // Store the updated users array in local storage
-  localStorage.setItem("users", JSON.stringify(users));
-
-  // Reload the user list
-  loadUserList();
-}
-
-// Function to load existing users from local storage and display them
-function loadUserList() {
-  axios
-    .get(
-      "https://crudcrud.com/api/d31be6839a4d404aa382199dbb392eeb/appointment"
-    )
-    .then((response) => {
-      const users = response.data;
-      const userList = document.getElementById("users");
-
-      // Clear the user list
-      userList.innerHTML = "";
-
-      // Display each user in the user list
-      users.forEach(function (user, index) {
-        const li = document.createElement("li");
-        li.appendChild(
-          document.createTextNode(
-            `Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}`
-          )
-        );
-
-        // Create the Edit button
-        const editButton = document.createElement("button");
-        editButton.innerHTML = "Edit";
-        editButton.addEventListener("click", function () {
-          editUser(index);
-        });
-        li.appendChild(editButton);
-
-        // Create delete button for each user
-        const deleteButton = document.createElement("button");
-        deleteButton.innerHTML = "Delete";
-        deleteButton.addEventListener("click", function () {
-          deleteUser(index);
-        });
-
-        // Append delete button to the list item
-        li.appendChild(deleteButton);
-        // Append list item to the user list
-        userList.appendChild(li);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
 
 // Load existing users from the API and display them
-document.addEventListener("DOMContentLoaded", function () {
-  loadUserList();
-});
+document.addEventListener("DOMContentLoaded", loadUserList);
